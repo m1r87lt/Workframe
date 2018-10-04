041857dc7b38f31f26abb79f0a0dc991c84e5bea
MLaRosa <marco.larosa@keypartner.it>
2018-10-04 22:26:53 +0200

    ...

--- Workframe/src/Object.cpp
+++ Workframe/src/Object.cpp
@@ -9,13 +9,163 @@
 
 namespace base {
 
-Object::~Object() {
-	// TODO Auto-generated destructor stub
-	std::clog << __func__ << "  }" << std::endl;
-}
-Object::Object(bool open, const Object* caller) {
-	// TODO Auto-generated constructor stub
+const std::string ns = "base";
+std::string substring_after(std::string string, std::string match,
+		size_t number = 1, bool from_end) {
 
 }
+
+//Object
+long long unsigned Object::tracker = 0;
+
+const std::string& Object::has_label() const {
+	return ns + "::" + label;
+}
+const std::string& Object::has_logger() const {
+	return logger;
+}
+std::string Object::makes_track(const Object* caller) {
+	std::string result;
+
+	if (caller)
+		result = caller->logger + ".";
+
+	return result + std::to_string(++tracker);
+}
+
+Object::Object(const Object* caller) {
+	// TODO Auto-generated constructor stub
+	logger = makes_track(caller);
+	label = ns + "::" + __func__;
+}
+Object::Object(const Object& copy) {
+	logger = makes_track(&copy);
+	label = copy.label;
+}
+Object::Object(Object&& moving) {
+	label = moving.label;
+	logger = moving.logger;
+	moving.logger.clear();
+}
+
+//Log
+std::ostringstream Log::log_arguments() {
+	return std::ostringstream();
+}
+void Log::log(std::string logger, std::string message, bool open,
+		const Object* returning) {
+	logger += ": ";
+
+	if (returning) {
+		if (open)
+			std::clog << logger << "  }=" << returning->prints().str()
+					<< std::endl;
+		else
+			std::clog << logger << message << "=" << returning->prints().str()
+					<< std::endl;
+	} else if (open)
+		std::clog << logger << message << " {" << std::endl;
+}
+void Log::log_unary(const Log& logging, std::type_index type,
+		const Object& operand, const Object* returning) {
+	log(logging.logger,
+			logging.prints().str() + "<" + type.name() + ">"
+					+ operand.prints().str(), open, returning);
+}
+void Log::log_unary(const Object& operand, const Log& logging,
+		std::type_index type, const Object* returning) {
+	log(logging.logger,
+			operand.prints().str() + "<" + type.name() + ">"
+					+ logging.prints().str(), logging.open, returning);
+}
+void Log::log_binary(const Log& logging, std::type_index type,
+		const Object& lefthand, const Object& righthand,
+		const Object* returning) {
+	log(logging.logger,
+			type.name() + " " + lefthand.prints().str()
+					+ (logging.label == "[]" ?
+							"[" + righthand.prints().str() + "]" :
+							" " + logging.prints().str() + " "
+									+ righthand.prints().str()), logging.open,
+			returning);
+}
+void Log::log_return(const Log& logging, Object& returning) {
+	log(logging.logger, "=" + returning.prints().str(), logging.open,
+			&returning);
+}
+Log Log::as_unary(const Object* caller, bool open, std::type_index type,
+		std::string operation, const Object& object) {
+	Log result(caller, operation, open);
+
+	log_unary(result, type, object, nullptr);
+
+	return result;
+}
+Log Log::as_unary(const Object* caller, bool open, std::type_index type,
+		const Object& object, std::string operation) {
+	Log result(caller, operation, open);
+
+	log_unary(object, result, type, nullptr);
+
+	return result;
+}
+Log Log::as_binary(const Object* caller, bool open, std::type_index type,
+		const Object& lefthand, std::string operation,
+		const Object& righthand) {
+	Log result(caller, operation, open);
+
+	log_binary(result, type, lefthand, righthand, nullptr);
+
+	return result;
+}
+void Log::notes(std::ostringstream message) const {
+	if (open)
+		std::clog << logger << "  " << message.str() << std::endl;
+}
+Object& Log::returns(Object& returning) const {
+	log_return(*this, returning);
+
+	return returning;
+}
+Object&& Log::returns(Object&& returning) const {
+	log_return(*this, returning);
+
+	return std::move(returning);
+}
+std::ostringstream Log::prints() const {
+	std::ostringstream result;
+
+	result << label << "{" << logger.s << "}";
+
+	return result;
+}
+void Log::logs_error(std::ostringstream message) const {
+	std::cerr << logger << ": " << label << message << std::endl;
+}
+Log Log::as_unary(const Object* caller, std::string operation,
+		std::type_index type) const {
+	return as_unary(caller, open, type, operation, *this);
+}
+Log Log::as_unary(const Object* caller, std::type_index type,
+		std::string operation) const {
+	return as_unary(caller, open, type, *this, operation);
+}
+Log Log::as_binary(const Object* caller, std::type_index type,
+		std::string operation, const Object& righthand) const {
+	return as_binary(caller, open, type, *this, operation, righthand);
+}
+
+Log::Log(const Object* caller, std::string label) {
+	// TODO Auto-generated constructor stub
+	logger = makes_log(caller);
+	this->label = label;
+}
+Log::~Log() {
+// TODO Auto-generated destructor stub
+}
+Log(const Log&);
+Log& operator =(const Log&);
+Log(Log&&);
+Log& operator =(Log&&);
 
 } /* namespace base */
--- Workframe/src/Object.h
+++ Workframe/src/Object.h
@@ -8,30 +8,100 @@
 #ifndef OBJECT_H_
 #define OBJECT_H_
 #include <string>
+#include <sstream>
+#include <typeindex>
 #include <iostream>
-#include <functional>
 
 namespace base {
 
+std::string substring_after(std::string, std::string, size_t, bool = false);
+
 class Object {
+	std::string label;
 	std::string logger;
-	bool open;
 	static long long unsigned tracker;
 public:
-	virtual ~Object();
-
-	virtual Object& operator ()(std::initializer_list<Object&>) = 0;
+	virtual std::ostringstream prints() const = 0;
 protected:
-	template<typename Argument, typename ... Argument> Object(bool open, const Object* caller, )
+	Object(const Object*);
+	virtual ~Object() = default;
+	Object(const Object&);
+	Object& operator =(const Object&) = delete;
+	Object(Object&&);
+	Object& operator =(Object&&) = delete;
 
-	Object(bool open, const Object* caller) :
-			open(open) {
-		std::clog << (logger = caller->logger + "." + std::to_string(++tracker))
-				<< ": " << __func__ << "{" << this << (open ? "}() {" : "}()")
-				<< std::endl;
+	const std::string& has_label() const;
+	const std::string& has_logger() const;
+	static std::string makes_track(const Object*, long long unsigned* = nullptr);
+};
+
+class Log: public Object {
+	long long unsigned track;
+	bool open;
+
+	template<typename ... Arguments> static std::ostringstream log_arguments(
+			Object& object, Arguments& ... arguments) {
+		return object.prints() << ", " << log_arguments(arguments ...);
+	}
+	static std::ostringstream log_arguments();
+	static void log(std::string, std::string, bool, const Object*);
+	static void log_unary(const Log&, std::type_index, const Object&,
+			const Object*);
+	static void log_unary(const Object&, const Log&, std::type_index,
+			const Object*);
+	static void log_binary(const Log&, std::type_index, const Object&,
+			const Object&, const Object*);
+	template<typename ... Arguments> static void log_method(const Log& logging,
+			std::type_index type, const Object* returning,
+			Arguments&& ... arguments) {
+		log(logging.logger,
+				logging.prints() + "<" + type.name() + ">("
+						+ log_arguments(arguments ...).str() + ")",
+				logging.open, returning);
+	}
+	static void log_return(const Log&, Object&);
+
+	Log(const Object*, std::string, bool);
+public:
+	static Log as_unary(const Object*, bool, std::type_index, std::string,
+			const Object&);
+	static Log as_unary(const Object*, bool, std::type_index, const Object&,
+			std::string);
+	static Log as_binary(const Object*, bool, std::type_index, const Object&,
+			std::string, const Object&);
+	template<typename ... Arguments> static Log as_method(const Object* caller,
+			bool open, std::string label, std::type_index type,
+			Arguments& ... arguments) {
+		Log result(caller, label, open);
+
+		log_method(result, type, nullptr, arguments ...);
+
+		return result;
+	}
+	void notes(std::ostringstream) const;
+	Object& returns(Object&) const;
+	Object&& returns(Object&&) const;
+	void logs_error(std::ostringstream) const;
+	virtual std::ostringstream prints() const;
+
+	virtual ~Log();
+	Log(const Log&);
+	Log& operator =(const Log&);
+	Log(Log&&);
+	Log& operator =(Log&&);
+protected:
+	Log as_unary(const Object*, std::string, std::type_index) const;
+	Log as_unary(const Object*, std::type_index, std::string) const;
+	Log as_binary(const Object*, std::type_index, std::string,
+			const Object&) const;
+	template<typename ... Arguments> Log as_method(const Object* caller,
+			std::string label, std::type_index type,
+			Arguments& ... arguments) const {
+		return as_method(caller, open, label, type, arguments ...);
 	}
 };
 
 } /* namespace base */
 
 #endif /* OBJECT_H_ */
+
