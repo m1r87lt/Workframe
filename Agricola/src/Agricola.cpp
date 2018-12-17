5a81cd9376399e67a14c043fe81dd38755547819
m1r <m1r@192.168.56.1>
2018-12-17 11:17:13 +0100

    ...

---
+++ Agricola/src/Agricola.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : Agricola.cpp
+// Author      : m1r
+// Version     : 0.1
+// Copyright   : 2018
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Agricola" << endl; // prints Agricola
+	return 0;
+}
